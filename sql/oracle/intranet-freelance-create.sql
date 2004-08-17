2b951213e7e12c55f80f03cf06742cc284c81b02
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2004-08-17 21:25:49 +0000

    - added recruitment fields to im_freelancers - modified im_view_columns to contain "order_by_clause" - modified /intranet/users/index.tcl to use the new order_by_clause   and to select a specific view for user groups (Freelancers etc.),   thus allowing for "dynamic subclassing".

--- sql/oracle/intranet-freelance-create.sql
+++ sql/oracle/intranet-freelance-create.sql
@@ -92,14 +92,23 @@ select category_id as skill_type_id, category as skill_type
 from im_categories 
 where category_type = 'Intranet Skill Type';
 
+-----------------------------------------------------------
+-- Menu Modifications
+--
+-- Let's redirect the "Users" / "Freelancers" menu
+-- to the local "index.tcl" page.
+update im_menus
+set url='/intranet-freelance/index'
+where label='users_freelancers';
 
+
+
+-----------------------------------------------------------
 -- We need to define this function as a type of "join(..., ", ") to
 -- get the list of skills for each user and skill type.
 --
 -- select im_freelance_skill_list(26,2000) from dual; -> 'es es_LA'
 --
-
-
 create or replace function im_freelance_skill_list ( 
 	p_user_id IN number,
 	p_skill_type_id IN number) 
@@ -128,8 +137,61 @@ END;
 show errors;
 
 
-insert into im_views (view_id, view_name, visible_for) values (50, 'user_list_freelance', 'view_users');
-insert into im_views (view_id, view_name, visible_for) values (51, 'user_view_freelance', 'view_users');
+insert into im_views (view_id, view_name, visible_for) values (50, 'user_list_freelance', '');
+insert into im_views (view_id, view_name, visible_for) values (51, 'user_view_freelance', '');
+insert into im_views (view_id, view_name, visible_for) values (52, 'freelancers_list', '');
+
+
+
+
+--------------------------------------------------------------
+-- FreelancersListPage
+--
+delete from im_view_columns where column_id >= 5200 and column_id < 5299;
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_from, extra_where, sort_order, visible_for) values (5200,52,NULL,'Name',
+'"<a href=/intranet/users/view?user_id=$user_id>$name</a>"',
+'f.bank_account, f.bank, f.payment_method_id, f.rec_source, 
+f.rec_status_id, f.rec_test_type, f.rec_test_result_id',
+'im_freelancers f','u.user_id = f.user_id',0,'');
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values (5201,52,NULL,'Email',
+'"<a href=mailto:$email>$email</a>"','','',2,'');
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values (5203,52,NULL,'MSM',
+'"<A HREF=\"http://arkansasmall.tcworks.net:8080/message/msn/$msn_email\">
+<IMG SRC=\"http://arkansasmall.tcworks.net:8080/msn/$msn_email\"
+width=21 height=22 border=0 ALT=\"MSN Status\"></A>"','','',3,'');
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values (5204,52,NULL,'Work Phone',
+'$work_phone','','',4,'');
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values (5205,52,NULL,'Cell Phone',
+'$cell_phone','','',5,'');
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values (5206,52,NULL,'Home Phone',
+'$home_phone','','',6,'');
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for, order_by_clause) values 
+(5208,52,NULL,'Recr Status','$rec_status',
+'im_category_from_id(rec_status_id) as rec_status','',8,'','order by rec_status');
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for, order_by_clause) values 
+(5210,52,NULL,'Recr Test','$rec_test_result',
+'im_category_from_id(rec_test_result_id) as rec_test_result','',10,'',
+'order by rec_test_result_id');
+--
+commit;
+
+
 
 
 -- Freelance Skill Types
@@ -176,6 +238,48 @@ INSERT INTO im_categories VALUES (2353,'Windows XP','','Intranet Operating Syste
 INSERT INTO im_categories VALUES (2354,'Linux','','Intranet Operating System','category','t','f');
 
 
+-- ------------------------------------------------------------
+-- Definition of Recruiting Categories
+-- ------------------------------------------------------------
+
+-- Intranet Recruiting Status
+insert into im_categories
+( CATEGORY_DESCRIPTION, ENABLED_P, CATEGORY_ID, CATEGORY, CATEGORY_TYPE)
+values ('', 'f', '6000', 'Potential Freelancer', 'Intranet Recruiting Status');
+
+insert into im_categories
+( CATEGORY_DESCRIPTION, ENABLED_P, CATEGORY_ID, CATEGORY, CATEGORY_TYPE)
+values ('', 'f', '6002', 'Test sent', 'Intranet Recruiting Status');
+
+insert into im_categories
+( CATEGORY_DESCRIPTION, ENABLED_P, CATEGORY_ID, CATEGORY, CATEGORY_TYPE)
+values ('', 'f', '6004', 'Test received', 'Intranet Recruiting Status');
+
+insert into im_categories
+( CATEGORY_DESCRIPTION, ENABLED_P, CATEGORY_ID, CATEGORY, CATEGORY_TYPE)
+values ('', 'f', '6006', 'Test evaluated', 'Intranet Recruiting Status');
+
+commit;
+
+
+
+-- Intranet Recruiting Test Results
+insert into im_categories
+( CATEGORY_DESCRIPTION, ENABLED_P, CATEGORY_ID, CATEGORY, CATEGORY_TYPE)
+values ('', 'f', '6100', 'A - Test approved', 'Intranet Recruiting Test Result');
+
+insert into im_categories
+( CATEGORY_DESCRIPTION, ENABLED_P, CATEGORY_ID, CATEGORY, CATEGORY_TYPE)
+values ('', 'f', '6102', 'B - No the best...', 'Intranet Recruiting Test Result');
+
+insert into im_categories
+( CATEGORY_DESCRIPTION, ENABLED_P, CATEGORY_ID, CATEGORY, CATEGORY_TYPE)
+values ('', 'f', '6104', 'C - Test completely failed', 'Intranet Recruiting Test Result');
+
+commit;
+
+
+
 
 -- Add 'user_list_freelance'
 delete from im_view_columns where column_id >= 5000 and column_id < 5099;
@@ -216,43 +320,56 @@ commit;
 delete from im_view_columns where column_id >= 5100 and column_id < 5199;
 
 insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
-extra_select, extra_where, sort_order, visible_for) values (5102,51,NULL,'Trans Rate',
-'$translation_rate','','',2,
+extra_select, extra_where, sort_order, visible_for) values (5102,51,NULL,'Recruiting Source',
+'$rec_source','','',2,'im_permission $user_id view_freelancers');
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values (5104,51,NULL,'Recruiting Status',
+'$rec_status','','',4,'im_permission $user_id view_freelancers');
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values (5106,51,NULL,'Recruiting Test Type',
+'$rec_test_type','','',6,'im_permission $user_id view_freelancers');
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values (5108,51,NULL,'Recruiting Test Result',
+'$rec_test_result','','',8,'im_permission $user_id view_freelancers');
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values (5112,51,NULL,'Trans Rate',
+'$translation_rate','','',12,
 'im_permission $user_id view_freelancers');
 
 insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
-extra_select, extra_where, sort_order, visible_for) values (5104,51,NULL,'Editing Rate',
-'$editing_rate','','',4,
+extra_select, extra_where, sort_order, visible_for) values (5114,51,NULL,'Editing Rate',
+'$editing_rate','','',14,
 'im_permission $user_id view_freelancers');
 
 insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
-extra_select, extra_where, sort_order, visible_for) values (5106,51,NULL,'Hourly Rate',
-'$hourly_rate','','',6,
+extra_select, extra_where, sort_order, visible_for) values (5116,51,NULL,'Hourly Rate',
+'$hourly_rate','','',16,
 'im_permission $user_id view_freelancers');
 
 insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
-extra_select, extra_where, sort_order, visible_for) values (5108,51,NULL,'Bank Account',
-'$bank_account','','',8,
+extra_select, extra_where, sort_order, visible_for) values (5118,51,NULL,'Bank Account',
+'$bank_account','','',18,
 'im_permission $user_id view_freelancers');
 
 insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
-extra_select, extra_where, sort_order, visible_for) values (5110,51,NULL,'Bank',
-'$bank','','',10,
+extra_select, extra_where, sort_order, visible_for) values (5120,51,NULL,'Bank',
+'$bank','','',20,
 'im_permission $user_id view_freelancers');
 
 insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
-extra_select, extra_where, sort_order, visible_for) values (5112,51,NULL,'Payment Method',
-'$payment_method','','',12,
+extra_select, extra_where, sort_order, visible_for) values (5132,51,NULL,'Payment Method',
+'$payment_method','','',22,
 'im_permission $user_id view_freelancers');
 
 insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
-extra_select, extra_where, sort_order, visible_for) values (5114,51,NULL,'Note',
-'<blockqote>$note</blockquote>','','',14,
+extra_select, extra_where, sort_order, visible_for) values (5124,51,NULL,'Note',
+'<blockqote>$note</blockquote>','','',24,
 'im_permission $user_id view_freelancers');
 
 insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
-extra_select, extra_where, sort_order, visible_for) values (5116,51,NULL,'Private Note',
-'<blockqote>$private_note</blockquote>','','',16,
+extra_select, extra_where, sort_order, visible_for) values (5126,51,NULL,'Private Note',
+'<blockqote>$private_note</blockquote>','','',26,
 'im_permission $user_id view_freelancers');
 
 commit;
