20d7e36af1b265a25cc9c059337eee1973ddb69f
lup <>
2005-12-20 19:19:00 +0000

    - added permissions to view and add freelance_skills

--- sql/postgresql/intranet-freelance-create.sql
+++ sql/postgresql/intranet-freelance-create.sql
@@ -197,6 +197,34 @@ select im_component_plugin__new (
 	'im_freelance_member_select_component $object_id $return_url'
     );
 
+
+
+
+-- -----------------------------------------------------
+-- Add privileges for freelance_skills and freelance_skills_hours
+--
+select acs_privilege__create_privilege('add_freelance_skills','Add Freelance Skills','Add Freelance Skills');
+select acs_privilege__add_child('admin', 'add_freelance_skills');
+
+select acs_privilege__create_privilege('view_freelance_skills','View Freelance Skills','View Freelance Skills');
+select acs_privilege__add_child('admin', 'view_freelance_skills');
+
+select im_priv_create('view_freelance_skills','Accounting');
+select im_priv_create('view_freelance_skills','P/O Admins');
+select im_priv_create('view_freelance_skills','Project Managers');
+select im_priv_create('view_freelance_skills','Senior Managers');
+select im_priv_create('view_freelance_skills','Freelance Managers');
+select im_priv_create('view_freelance_skills','Employees');
+
+select im_priv_create('add_freelance_skills','Accounting');
+select im_priv_create('add_freelance_skills','P/O Admins');
+select im_priv_create('add_freelance_skills','Senior Managers');
+select im_priv_create('add_freelance_skills','Project Managers');
+select im_priv_create('add_freelance_skills','Freelance Managers');
+
+
+
+
 \i intranet-freelance-score.sql
 \i ../common/intranet-freelance-common.sql
 \i ../common/intranet-freelance-backup.sql
