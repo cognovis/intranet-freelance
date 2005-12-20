f48e5d230e622b408e5d4648cfeae91fe5c0249d
lup <>
2005-12-20 19:26:40 +0000

    - added "freelance_skillconfs" Skill confirmations privilege

--- sql/postgresql/intranet-freelance-create.sql
+++ sql/postgresql/intranet-freelance-create.sql
@@ -201,7 +201,7 @@ select im_component_plugin__new (
 
 
 -- -----------------------------------------------------
--- Add privileges for freelance_skills and freelance_skills_hours
+-- Add privileges for freelance_skills and freelance_skillconfs
 --
 select acs_privilege__create_privilege('add_freelance_skills','Add Freelance Skills','Add Freelance Skills');
 select acs_privilege__add_child('admin', 'add_freelance_skills');
@@ -221,6 +221,30 @@ select im_priv_create('add_freelance_skills','P/O Admins');
 select im_priv_create('add_freelance_skills','Senior Managers');
 select im_priv_create('add_freelance_skills','Project Managers');
 select im_priv_create('add_freelance_skills','Freelance Managers');
+
+select im_priv_create('view_freelance_skills','Freelancers');
+select im_priv_create('add_freelance_skills','Freelancers');
+
+
+
+select acs_privilege__create_privilege('add_freelance_skillconfs','Add Freelance Skillconfs','Add Freelance Skillconfs');
+select acs_privilege__add_child('admin', 'add_freelance_skillconfs');
+
+select acs_privilege__create_privilege('view_freelance_skillconfs','View Freelance Skillconfs','View Freelance Skillconfs');
+select acs_privilege__add_child('admin', 'view_freelance_skillconfs');
+
+select im_priv_create('view_freelance_skillconfs','Accounting');
+select im_priv_create('view_freelance_skillconfs','P/O Admins');
+select im_priv_create('view_freelance_skillconfs','Project Managers');
+select im_priv_create('view_freelance_skillconfs','Senior Managers');
+select im_priv_create('view_freelance_skillconfs','Freelance Managers');
+select im_priv_create('view_freelance_skillconfs','Employees');
+
+select im_priv_create('add_freelance_skillconfs','Accounting');
+select im_priv_create('add_freelance_skillconfs','P/O Admins');
+select im_priv_create('add_freelance_skillconfs','Senior Managers');
+select im_priv_create('add_freelance_skillconfs','Project Managers');
+select im_priv_create('add_freelance_skillconfs','Freelance Managers');
 
 
 
--- sql/postgresql/upgrade/upgrade-3.1.0.0.0-3.1.0.1.0.sql
+++ sql/postgresql/upgrade/upgrade-3.1.0.0.0-3.1.0.1.0.sql
@@ -23,5 +23,29 @@ select im_priv_create('add_freelance_skills','Senior Managers');
 select im_priv_create('add_freelance_skills','Project Managers');
 select im_priv_create('add_freelance_skills','Freelance Managers');
 
+select im_priv_create('view_freelance_skills','Freelancers');
+select im_priv_create('add_freelance_skills','Freelancers');
+
+
+
+select acs_privilege__create_privilege('add_freelance_skillconfs','Add Freelance Skillconfs','Add Freelance Skillconfs');
+select acs_privilege__add_child('admin', 'add_freelance_skillconfs');
+
+select acs_privilege__create_privilege('view_freelance_skillconfs','View Freelance Skillconfs','View Freelance Skillconfs');
+select acs_privilege__add_child('admin', 'view_freelance_skillconfs');
+
+select im_priv_create('view_freelance_skillconfs','Accounting');
+select im_priv_create('view_freelance_skillconfs','P/O Admins');
+select im_priv_create('view_freelance_skillconfs','Project Managers');
+select im_priv_create('view_freelance_skillconfs','Senior Managers');
+select im_priv_create('view_freelance_skillconfs','Freelance Managers');
+select im_priv_create('view_freelance_skillconfs','Employees');
+
+select im_priv_create('add_freelance_skillconfs','Accounting');
+select im_priv_create('add_freelance_skillconfs','P/O Admins');
+select im_priv_create('add_freelance_skillconfs','Senior Managers');
+select im_priv_create('add_freelance_skillconfs','Project Managers');
+select im_priv_create('add_freelance_skillconfs','Freelance Managers');
+
 
 
