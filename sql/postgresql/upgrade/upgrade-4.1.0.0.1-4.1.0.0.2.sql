c95e6698265f714d4d6871fc2097ff9909959efc
Malte Sussdorff <malte.sussdorff@cognovis.com>
2015-02-04 18:29:47 +0100

    Added menu to the skill project type match

---
+++ sql/postgresql/upgrade/upgrade-4.1.0.0.1-4.1.0.0.2.sql
@@ -0,0 +1,3 @@
+SELECT acs_log__debug('/packages/intranet-freelance/sql/postgresql/upgrade/upgrade-4.1.0.0.1-4.1.0.0.2.sql','');
+
+select im_menu__new(NULL,'acs_object',now(),NULL,NULL,NULL,'intranet-freelance','freelance-skills','Freelance Skills','/intranet-freelance/object-types','200','482',NULL);
